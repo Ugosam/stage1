4c2961ad36d24209578f7dd34ee9efe14c68884a
Ugosam <ugosamuel33@gmail.com>
2023-09-11 13:36:39 +0100

    second-commit

--- myScript.js
+++ myScript.js
@@ -1,23 +1,27 @@
 // days of the week
-        const DaysOfTheWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+const DaysOfTheWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
-        const Time = new Date();
-        // console.log(Time);
+const Time = new Date();
+// console.log(Time);
 
-        const Day = Time.getDay();
-        // console.log(Day);
+const Day = Time.getDay();
+// console.log(Day);
 
-        const currentDay = DaysOfTheWeek[Day];
+const currentDay = DaysOfTheWeek[Day];
 
-         document.getElementById('currentDay').textContent = `Current Day of the Week: ${currentDay}`;
+document.getElementById('currentDay').textContent = `Current Day of the Week: ${currentDay}`;
 
 
 
-         const timeOut = new Date();
-         
 
-         const UTCMilli = timeOut.getMilliseconds();
+function Second() {
+    const timeOut = new Date();
+    const UTCMilli = timeOut.getTime();
 
-         document.getElementById('milliSeconds').textContent = `Current UTC time in milliseconds is: ${UTCMilli}`;
+    document.getElementById('milliSeconds').textContent = `Current UTC time in milliseconds is: ${UTCMilli}`;
+
+}
+  const  UTCMilliSecond = setInterval(Second, 1);
+
 
 
